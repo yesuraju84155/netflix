c50513e8165557cae7826c722bb722c355f7fa86
4727yesuraju <94819056+4727yesuraju@users.noreply.github.com>
2024-09-27 14:50:51 +0530

    Update cron.js
    cron uri is updated

--- backend/cron/cron.js
+++ backend/cron/cron.js
@@ -1,7 +1,7 @@
 import cron from "cron";
 import https from "https";
 
-const URL = "https://yesu-threads.onrender.com";
+const URL = "https://yesu-netflix.onrender.com/";
 
 const job = new cron.CronJob("*/14 * * * *", function () {
 	https
